e1221389b04552e590da4bbb3647fe0061f659ed
GHT HuyPQ <>
2024-06-08 10:57:21 +0700

    push

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
-    alias(libs.plugins.androidApplication)
-    alias(libs.plugins.jetbrainsKotlinAndroid)
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("maven-publish")
 }
 
 android {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,10 +15,11 @@ dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
-        maven("https://jitpack.io")
         mavenCentral()
+        maven("https://jitpack.io")
     }
 }
+
 
 rootProject.name = "AndroidComponent"
 include(":app")
